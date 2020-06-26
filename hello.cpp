2688bba6b7a54f209c88d01a86d6c1eb12dc626d
Ioan Rusu <IOAN-RAZVAN.RUSU@my.fmi.unibuc.ro>
2020-06-26 15:23:24 +0300

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char ** argv)
+{
+  if (argc < 2) {
+    std::cerr << "There weren't any names provided!";
+    std::exit(EXIT_FAILURE);
+  }
+  std::cout << "Hello " + argv[1];
+  return 0;
+}
